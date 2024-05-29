b3e1891e6104f716e20c7925c7a37c09441c1968
MayankCode12 <162474277+MayankCode12@users.noreply.github.com>
2024-05-29 22:52:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const movieGrid = document.getElementById("movie-grid");
+const moviePosters = [
+  {
+    id: 1,
+    title: "Movie 1",
+    poster: "movie-poster-1.jpg",
+    synopsis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit...",
+    genre: "Action",
+    director: "andrew",
+    cast: ["Actor 1", "Actor 2", "Actor 3"],
+    rating: 8.5,
+  },
+  {
+
+  
+
+  
+    id: 2
+  
